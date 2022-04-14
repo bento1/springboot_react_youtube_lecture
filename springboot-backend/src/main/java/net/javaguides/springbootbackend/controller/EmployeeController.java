b7d4b781178905acc917e1cd8a35eea9bea650f7
bento1 <bentotoda@naver.com>
2022-04-14 23:17:44 +0900

    first development
    add employee
    update information
    backend, frontend

---
+++ springboot-backend/src/main/java/net/javaguides/springbootbackend/controller/EmployeeController.java
@@ -0,0 +1,48 @@
+package net.javaguides.springbootbackend.controller;
+
+
+import net.javaguides.springbootbackend.exception.ResourceNotFoundException;
+import net.javaguides.springbootbackend.model.Employee;
+import net.javaguides.springbootbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@CrossOrigin(origins="http://localhost:3000")//Access to XMLHttpRequest at 'http://localhost:8080/api/v1/employees' from origin 'http://localhost:3000' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+    //create employee rest api
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    //get employy by id rest api
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id){
+        Employee employee=  employeeRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Employee not exist with id :" +id));
+        return  ResponseEntity.ok(employee);
+    }
+
+    //update employee rest api
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable long id,@RequestBody Employee employeeDetails){
+        Employee employee=  employeeRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Employee not exist with id :" +id));
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+        Employee updatedEmployee=employeeRepository.save(employee);
+        return  ResponseEntity.ok(updatedEmployee);
+        //header에서 content-type : application/json 설정하고 넣어야함
+    }
+}
